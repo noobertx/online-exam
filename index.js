500b173230b06f2b485bceeadb1eb14035ac1713
noobertx <noobertx@gmail.com>
2020-01-08 00:01:58 +0800

    added static folder and prep SPA

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/api/members',require('./routes/api/members'));
 
+// Handle Production
+if(process.env.NODE_ENV==='production'){
+	app.use(express.static(__dirname+'public'));
+	app.get(/.*/,(req,res)=>res.sendFile(__dirname+'/public/index.html'));
+}
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
