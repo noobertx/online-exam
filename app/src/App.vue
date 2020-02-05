93071de8b03ab6b14b30fe3523c9b1eacce6815e
noobertx <noobertx@gmail.com>
2020-02-06 03:28:40 +0800

    Admin UI Revision

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="container">      
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">Online Exam</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
--- app/src/components/QuizItemComponent.vue
+++ app/src/components/QuizItemComponent.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="wrapper">
+  <div class="wrapper" style="
+    background: #fff;
+    overflow: hidden;
+">
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
