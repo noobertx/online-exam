b244b59675106bd0b372bbd417ea1f42044138ae
noobertx <noobertx@gmail.com>
2020-02-07 01:32:00 +0800

    Initial API Filtering

--- services/Quiz.services.js
+++ services/Quiz.services.js
@@ -18,8 +18,32 @@ exports.createQuiz = async (req,res)=>{
 }
 
 exports.getAllQuiz = async(req,res)=>{
+	
+
+	class QuizAPIFeatures{
+		constructor(query,queryString){
+			this.query=query;
+			this.queryString=queryString;
+		}
+
+		filter(){
+			const queryObj = {...this.queryString}
+			const excludedFields = ['page','sort','limit','fields']
+			excludedFields.forEach(el=> delete queryObj[el])
+			
+			// 1b) Advance Filtering
+			let queryStr = JSON.stringify(queryObj)
+			queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g math=> `$${match}`)
+			
+			this.query = this.query.find(JSON.parse(queryStr))
+			return this;
+		}
+	}
+
 	try{		
-	const quiz = await Quiz.find();
+	const features = new QuizAPIFeatures(Quiz.find(),req.query).filter();
+	const quiz = await features.query
+	// const quiz = await Quiz.find();
 	res.status(200).json(quiz)
 	}catch(err){
 		res.status(404).json({
