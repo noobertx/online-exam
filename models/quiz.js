e46011ebf51624c2e074ccaaf02d27f32ac561cc
noobertx <noobertx@gmail.com>
2020-02-07 02:37:27 +0800

    Adding in Virtual Properties Total Points

--- models/quiz.js
+++ models/quiz.js
@@ -8,7 +8,22 @@ const quizSchema =  new mongoose.Schema({
 	quizItems:Array,
 	settings:mongoose.Schema.Types.Mixed,
 	meta:mongoose.Schema.Types.Mixed
+},{
+	toJSON:{virtuals:true},
+	toObject:{virtuals:true}
 })
+// Virtual Properties
+
+
+	quizSchema.virtual('totalScore').get(function(){
+		var total_score = 0;
+		this.quizItems.forEach((q)=>{
+			total_score += parseInt(q.points)
+		})
+		return total_score;
+	})
+
+
 
 const Quiz = mongoose.model('Quiz',quizSchema)
 
