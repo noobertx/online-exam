237597fc6b9e3c2153dfd2b5663cc7d8b6ef87d3
noobertx <noobertx@gmail.com>
2020-02-07 03:24:17 +0800

    Added Basic Validator to quiz model

--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const quizSchema =  new mongoose.Schema({	
-	title:String,
+	title:{
+		type:String,
+		maxlength:[50,"Title must have 50 or less characters"],
+		minlength:[5,"Title must have a character of more than 5 characters"]
+	},
 	intro:String,
 	tag:String,
 	date:Date,
@@ -23,8 +27,16 @@ const quizSchema =  new mongoose.Schema({
 		return total_score;
 	})
 
+// Document Middleware : runs before save and create
+// quizSchema.pre('save',function(next){})
+// quizSchema.post('save',function(doc , next){ })
+
+//	QUERY MIDDLEWARE
+// quizSchema.pre('find',function(next){})
 
 
 const Quiz = mongoose.model('Quiz',quizSchema)
 
 module.exports = Quiz;
+
+// MiddleWares
