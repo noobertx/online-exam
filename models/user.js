f44a7a7bb027fabf45a6384b8f7422dd047f88ee
noobertx <noobertx@gmail.com>
2020-02-08 07:38:33 +0800

    Password Hashing Implemented

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
 	name:{
@@ -27,9 +28,20 @@ const userSchema = new mongoose.Schema({
 	},
 	passwordConfirm:{
 		type:String,
-		required:[true,"Please Confirm your password"]
+		required:[true,"Please Confirm your password"],
+		validate:{
+			validator:function(el){
+				return this.password === el
+			}
+		}
 	},
+})
 
+userSchema.pre('save',async function(next){
+	if(!this.isModified('password')) return	next();
+	this.password = await bcrypt.hash(this.password,12)
+	this.passwordConfirm = undefined;
+	next();
 })
 
 const User = mongoose.model('User',userSchema)
