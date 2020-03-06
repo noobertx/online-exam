9f86950f812d92aa63dc7c58d6480da540a63553
noobertx <noobertx@gmail.com>
2020-03-07 00:31:00 +0800

    Singed Up user with token

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports={
-	outputDir:path.resolve(__dirname,'mobile-cta-client'),
+	outputDir:path.resolve(__dirname,'app'),
 	devServer:{
 		proxy:{
 			'/api':{
--- services/auth-user.services.js
+++ services/auth-user.services.js
@@ -1,10 +1,23 @@
 const User = require("./../models/user.js");
 const catchAsync = require("./catchAsync.service");
+const jwt = require("jsonwebtoken");
 
 exports.signup = catchAsync( async(req,res,next) => {
-	const newUser = await User.create(req.body);
+	const newUser = await User.create({
+		name:req.body.name,
+		email:req.body.email,
+		password:req.body.password,
+		passwordConfirm:req.body.passwordConfirm
+	});
+
+
+	const token = jwt.sign({id:newUser._id},process.env.REFRESH_TOKEN_SECRET,{
+		expiresIn:process.env.TOKEN_EXPIRES_IN
+	});
+
 	res.status(201).json({
 		status:'success',
+		token,
 		data:{
 			user: newUser
 		}
