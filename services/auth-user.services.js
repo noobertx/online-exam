2a78e101c78b6a98c2f959d47e2c8237f5d1e0e5
noobertx <noobertx@gmail.com>
2020-03-10 16:19:09 +0800

    Password reset working

--- services/auth-user.services.js
+++ services/auth-user.services.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const {promisify} = require("util");
 const User = require("./../models/user.js");
 const catchAsync = require("./catchAsync.service");
@@ -145,6 +146,33 @@ exports.forgotPassword =catchAsync(async (req,res,next)  => {
 
 	
 })
-exports.resetPassword = catchAsync(async(req,res,next)  => {
-	console.log(req)
+exports.resetPassword = catchAsync( async (req,res,next)  => {
+	const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+
+	const user =  await User.findOne({passwordResetToken:hashedToken,passwordResetExpires : { $gt:Date.now() }});
+
+
+
+
+	if(!user){
+		console.log("Error")
+		return next(new AppError("Token is invalid or expired",404))
+	}
+
+	user.password = req.body.password;
+	user.passwordConfirm = req.body.passwordConfirm;
+	user.passwordResetToken = undefined;
+	user.passwordResetExpires = undefined;
+
+	await user.save();
+
+	const token = signToken(user._id)
+
+	res.status(200).json({
+		status:'success',
+		token,
+		data:{
+			user: user
+		}
+	})
 })
